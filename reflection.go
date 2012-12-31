511c9c2ddd4e3e099b2bc113b3156b470af84d3b
M@ Dunlap <matthew.dunlap+hrilab@gmail.com>
2012-12-31 00:20:02 -0500

    fixed a panic that happened when repeatovers were nested and the new data wasnt a struct

--- reflection.go
+++ reflection.go
@@ -21,9 +21,12 @@ func getBool(name string, cont *context) (bool, error) {
 	if cont == nil {
 		return false, fmt.Errorf("No field named %v in the data (or no data provided)", name)
 	}
-	field := reflect.ValueOf(cont.data).Elem().FieldByName(name)
-	if field.IsValid() {
-		return field.Bool(), nil
+	switch reflect.ValueOf(cont.data).Kind().String() {
+	case "ptr":
+		field := reflect.ValueOf(cont.data).Elem().FieldByName(name)
+		if field.IsValid() {
+			return field.Bool(), nil
+		}
 	}
 	return getBool(name, cont.enclosure)
 }
@@ -32,9 +35,12 @@ func getLength(name string, cont *context) (int, error) {
 	if cont == nil {
 		return -1, fmt.Errorf("No field named %v in the data (or no data provided)", name)
 	}
-	field := reflect.ValueOf(cont.data).Elem().FieldByName(name)
-	if field.IsValid() {
-		return field.Len(), nil
+	switch reflect.ValueOf(cont.data).Kind().String() {
+	case "ptr":
+		field := reflect.ValueOf(cont.data).Elem().FieldByName(name)
+		if field.IsValid() {
+			return field.Len(), nil
+		}
 	}
 	return getLength(name, cont.enclosure)
 }
@@ -43,12 +49,15 @@ func getItem(name string, index int, cont *context) (*context, error) {
 	if cont == nil {
 		return nil, fmt.Errorf("No field named %v in the data (or no data provided)", name)
 	}
-	field := reflect.ValueOf(cont.data).Elem().FieldByName(name)
-	if field.IsValid() {
-		if 0 <= index && index <= field.Len() {
-			return makeContext(field.Index(index).Interface(), cont), nil
+	switch reflect.ValueOf(cont.data).Kind().String() {
+	case "ptr":
+		field := reflect.ValueOf(cont.data).Elem().FieldByName(name)
+		if field.IsValid() {
+			if 0 <= index && index <= field.Len() {
+				return makeContext(field.Index(index).Interface(), cont), nil
+			}
+			return nil, fmt.Errorf("Index %v out of bounds.", index)
 		}
-		return nil, fmt.Errorf("Index %v out of bounds.", index)
 	}
 	return getItem(name, index, cont.enclosure)
 }
